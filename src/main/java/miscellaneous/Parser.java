9973a51264a739aca4dba450452b624e6bc9f32e
Basudeb2005 <e1303027@u.nus.edu>
2025-03-30 19:34:47 +0800

    Fix Parser.extractValue to include prescription parameter prefixes

--- src/main/java/miscellaneous/Parser.java
+++ src/main/java/miscellaneous/Parser.java
@@ -276,7 +276,8 @@ public class Parser {
         }
 
         start += prefix.length();
-        String[] possible = {"n/", "ic/", "dob/", "g/", "p/", "a/", "dt/", "t/", "dsc/", "h/", "old/", "new/"};
+        // Add prescription-specific prefixes s/, m/, and nt/ to the list of possible prefixes
+        String[] possible = {"n/", "ic/", "dob/", "g/", "p/", "a/", "dt/", "t/", "dsc/", "h/", "old/", "new/", "s/", "m/", "nt/"};
         int end = input.length();
 
         // Determine where the current parameter's detail ends by finding the start of the next parameter
